8fad05a24e67f27c1ba582702f3ebb408a975aa7
RizalRizzler <mr995750@gmail.com>
2025-11-29 15:38:29 +0700

    Now using parcelable on Pokemon.kt/tweaked constructor function

--- app/src/main/java/com/example/myapplication/models/Pokemon.kt
+++ app/src/main/java/com/example/myapplication/models/Pokemon.kt
@@ -1,89 +1,94 @@
 package com.example.myapplication.models
 
-class Pokemon(val species: pokemonSpecies){
-    var name: String = species.name
-    var maxHP: Int = species.baseHP
-    val moves: List<Move> = species.moves
+import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 
-    private  var _currentHp: Int = maxHP
+@Parcelize
+class Pokemon(
+    val speciesName: String,
+    var level: Int = 5,
+    var exp: Int = 0,
+    private var _currentHp: Int
+) : Parcelable {
+
+    @IgnoredOnParcel
+    val species: pokemonSpecies
+        get() = Pokedex.database[speciesName]?: throw IllegalStateException("Species '$speciesName' not found in Pokedex!")
+
+    @IgnoredOnParcel
+    val maxHP: Int
+        get() = species.baseHP + (level - 1) * 3
+    @IgnoredOnParcel
+    val currentAtk: Int
+        get() = species.baseATK + (level - 1) * 2
+
     val currentHP: Int
         get() = _currentHp
 
-    //starter lv set to 5
-    var level: Int = 5
-        private set
+    val moves: List<Move>
+        get() = species.moves
 
-    var exp: Int = 0
-        private set
+    val name: String
+        get() = species.name
 
-    val expToLevelUp: Int
-        get() = level * level * level
-
-    var currentAtk: Int = species.baseATK +  (level*2)
-//        get() = species.baseATK +  (level*2)
-
-    init {
-        _currentHp = maxHP
-    }
+    // 4. Logic Methods
     fun takeDamage(damage: Int){
         _currentHp -= damage
-        if (_currentHp < 0){
-            _currentHp = 0
-        }
+        if (_currentHp < 0) _currentHp = 0
     }
 
     fun heal(amount: Int){
         _currentHp += amount
-        if (_currentHp >maxHP){
-            _currentHp = maxHP
-        }
+        if (_currentHp > maxHP) _currentHp = maxHP
     }
 
     fun isFainted(): Boolean = _currentHp <= 0
-    //println to string builder
+
     fun expGain(amount: Int): String {
         exp += amount
         val logBuilder = StringBuilder("$name gained $amount EXP.")
-
         while (exp >= expToLevelUp) {
-            val levelUpMessage = levelUp()
-            logBuilder.append("\n").append(levelUpMessage)
+            logBuilder.append("\n").append(levelUp())
         }
-
         return logBuilder.toString()
     }
+
+    val expToLevelUp: Int
+        get() = level * level * level
 
     fun levelUp(): String{
         exp -= expToLevelUp
         level++
-    
-        
-        maxHP = species.baseHP + (level - 1) * 3
-        currentAtk = species.baseATK + (level - 1) * 2
-        //heal ke maxHP
+
         _currentHp = maxHP
+
         return "$name grew to Level $level! (HP: $maxHP, Atk: $currentAtk)"
     }
 
     fun setLevel(newLvl: Int){
         level = newLvl.coerceAtLeast(1)
         exp = 0
-
-        maxHP = species.baseHP + (level - 1) * 3
-        currentAtk = species.baseATK + (level - 1) * 2
         _currentHp = maxHP
-
     }
 }
 
+//ganti construct
 
 fun createPokemon(speciesName: String): Pokemon? {
-    val species = Pokedex.database[speciesName.uppercase()]?: return null
-    return Pokemon(species)
+    val species = Pokedex.database[speciesName.uppercase()] ?: return null
+
+    val startingLevel = 5
+    val initialMaxHP = species.baseHP + (startingLevel - 1) * 3
+    return Pokemon(
+        speciesName = speciesName.uppercase(),
+        level = startingLevel,
+        exp = 0,
+        _currentHp = initialMaxHP
+    )
 }
 
 fun createRandomWildPokemon(): Pokemon {
     val randomSpeciesName = Pokedex.database.keys.random()
     return createPokemon(randomSpeciesName)!!
 }
-
